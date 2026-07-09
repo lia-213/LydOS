8e93662d80d1de8eb03f2c50236fc1d31aa4caab
lia-213 <lydbo213@gmail.com>
2026-07-09 22:20:29 +0100

    content-addressable storage: finding the blob based on the content of the blob itself

--- ugit/data.py
+++ ugit/data.py
@@ -1,9 +1,24 @@
+import hashlib
 import os
 
 GIT_DIR = '.ugit'
 
 def init():
+    objects_dir = os.path.join(GIT_DIR, 'objects')
+
     if os.path.exists(GIT_DIR):
         print("(Bonus!) Reinitialised existing ugit repository!")
     else:
-        os.makedirs(GIT_DIR, exist_ok=True)
+        # creates both .ugit (GIT_DIR) and .ugit/objects (join(GIT_DIR, 'objects')) as it creates the latter recursively
+        os.makedirs(objects_dir, exist_ok=True)
+
+def hash_object(data):
+    oid = hashlib.sha256(data).hexdigest()
+
+    # .ugit/objects/the_hash_string
+    object_path = os.path.join(GIT_DIR, 'objects', oid)
+
+    with open(object_path, 'wb') as out:
+        out.write(data)
+    
+    return oid
