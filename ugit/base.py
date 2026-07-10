b65c2bfd41e415534a1ddbe481a272e0a730b47d
lia-213 <lydbo213@gmail.com>
2026-07-10 16:55:48 +0100

    write-tree: ignore .ugit files

--- ugit/base.py
+++ ugit/base.py
@@ -1,15 +1,23 @@
 import os
 
 from . import data
+from pathlib import Path
 
 def write_tree(directory='.'):
     """returns an OID"""
     with os.scandir(directory) as it:
         for entry in it:
             full = os.path.join(directory, entry.name)
+
+            if is_ignored(full):
+                continue
+
             if entry.is_file(follow_symlinks=False):
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
 
     # TODO: create tree object
+
+def is_ignored(path):
+    return '.ugit' in Path(path).parts
