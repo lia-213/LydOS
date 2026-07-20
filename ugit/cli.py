d2777af6583f5c168ecc145536824363befa73ce
Lydia K <lydbo213@gmail.com>
2026-07-20 16:27:50 +0100

    implemented git checkout: read tree and move HEAD to that hash

--- ugit/cli.py
+++ ugit/cli.py
@@ -3,7 +3,6 @@
 import argparse
 import os
 import sys
-import textwrap
 
 from . import data
 from . import base
@@ -40,10 +39,6 @@ def parse_args():
     commit_parser.set_defaults(func=commit)
     commit_parser.add_argument('-m', '--message', required=True)
 
-    log_parser = commands.add_parser('log')
-    log_parser.set_defaults(func=log)
-    log_parser.add_argument('oid', nargs='?')
-
     return parser.parse_args()
 
 def init(args):
@@ -69,14 +64,3 @@ def read_tree(args):
 
 def commit(args):
     print(base.commit(args.message))
-
-def log(args):
-    oid = args.oid or data.get_HEAD()
-    while oid:
-        commit = base.get_commit(oid)
-
-        print(f'commit {oid}\n')
-        print(textwrap.indent(commit.message, '     '))
-        print('')
-
-        oid = commit.parent
