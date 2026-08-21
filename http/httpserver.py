8f872004db59b5331e7b8bb78c8ed79dfe367a27
Lydia K <lydbo213@gmail.com>
2026-08-21 21:04:24 +0100

    implemented sending http responses using sockets and returning Index.html when a browser requests the root of the server (using a HTTP request e.g. GET / HTTP/1.0) by default

---
+++ http/httpserver.py
@@ -0,0 +1,45 @@
+"""
+ Implements a simple HTTP/1.0 Server
+
+"""
+
+import socket
+
+
+# Define socket host and port
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 8000
+
+# Create socket
+# set server_socket variable to AF_INET (IPv4 address family) and SOCK_STREAM (~TCP)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.listen(1)
+print('Listening on port %s ...' % SERVER_PORT)
+
+while True:
+    # Wait for client connections
+    client_connection, client_address = server_socket.accept()
+
+    # Get the client request (read the request string)
+    request = client_connection.recv(1024).decode()
+    print(request)
+
+    # Get the content of htdocs/index.html - read the contents of the file and add it to the response string as a message body
+    fin = open('htdocs/index.html')
+    content = fin.read()
+    fin.close()
+
+    # Send HTTP response
+    response = 'HTTP/1.0 200 OK\n\n' + content
+    client_connection.sendall(response.encode())
+    # Close client connnection
+    client_connection.close()
+
+# Close socket
+server_socket.close()
+
+"""whatever string you put in the response body gets sent verbatim to the browser, and the browser interprets it as HTML.
+    """
+#root of a server request: GET / HTTP/1.0, which should return the index.html page by default
